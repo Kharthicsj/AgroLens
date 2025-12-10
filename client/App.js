2b01fb70b5fd1795d587ce57a7d54ec5588c48f1
Kharthic S J <kharthicsj@gmail.com>
2025-12-10 15:50:19 +0530

    Remove problematic font loading causing infinite loading

--- client/App.js
+++ client/App.js
@@ -2,32 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import AppRouter from './router/route';
-import { useEffect, useState } from 'react';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import { View, ActivityIndicator } from 'react-native';
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
-
-  useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        ...Ionicons.font,
-      });
-      setFontsLoaded(true);
-    }
-    loadFonts();
-  }, []);
-
-  if (!fontsLoaded) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000' }}>
-        <ActivityIndicator size="large" color="#667eea" />
-      </View>
-    );
-  }
-
   return (
     <ThemeProvider>
       <AuthProvider>
