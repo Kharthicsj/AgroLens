5ef843f197f63f0be7005dbfba5ba451c6cd7044
Kharthic S J <kharthicsj@gmail.com>
2026-02-16 22:28:19 +0530

    Fix mail

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -9,9 +9,6 @@ const createTransporter = () => {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             refreshToken: process.env.GOOGLE_REFRESH_TOKEN
-        },
-        tls: {
-            rejectUnauthorized: false
         }
     });
 };
